e6dfa3aab695a4732866bd87db241c771d08d5c8
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2023-10-14 16:48:55 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACbV2lLo2nMkscl77CKQ8CefjJBmXB73s",
-  authDomain: "project-1dee7.firebaseapp.com",
-  projectId: "project-1dee7",
-  storageBucket: "project-1dee7.appspot.com",
-  messagingSenderId: "198381421185",
-  appId: "1:198381421185:web:e8a0fcfe06e109bca48380",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
